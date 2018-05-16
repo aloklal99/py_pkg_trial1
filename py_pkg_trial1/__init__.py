6082cdabfa66e34eee44710422c4d057daaa45fa
allal <allal@ebay.com>
2018-05-15 17:53:19 -0700

    Initial commit

---
+++ py_pkg_trial1/__init__.py
@@ -0,0 +1,2 @@
+def joke():
+    return "This is py_pkg_trial1 package!"
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='py_pkg_trial1',
+      version='0.1',
+      description='Trial package',
+      url='http://github.com/storborg/funniest',
+      author='Alok Lal',
+      author_email='allal@ebay.com',
+      license='MIT',
+      packages=['py_pkg_trial1'],
+      zip_safe=False)
